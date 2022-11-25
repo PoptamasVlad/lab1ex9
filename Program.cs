98a4d1088fde4851d74cc47f6bc55d20b054a38f
PoptamasVlad <118806771+PoptamasVlad@users.noreply.github.com>
2022-11-25 21:09:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care interschimba valoarea a doua variabile intregi.*/
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int x;
+
+            x = num1;
+            num1 = num2;
+            num2 = x;
+
+            Console.WriteLine(num1 + " " + num2);
+
+        }
+    }
+}
